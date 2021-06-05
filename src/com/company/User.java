c5bcdfac7a9ba791518cc3b0d4df55c4c19cf729
LucasMostafa <mostafalucas@gmail.com>
2021-06-05 02:33:48 -0300

    Funcion ModifyUser

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -120,30 +120,6 @@ public class User {
     }
 
     ///Metodos
-
-    public void register(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Nombre: ");
-        this.name = scanner.nextLine();
-        System.out.println("Apellido: ");
-        this.surname = scanner.nextLine();
-        System.out.println("DNI: ");
-        this.dni = scanner.nextLine();
-        System.out.println("Género: ");
-        this.gender = scanner.nextLine();
-        System.out.println("País de Origen: ");
-        this.origin = scanner.nextLine();
-        System.out.println("Dirección: ");
-        this.address = scanner.nextLine();
-        System.out.println("Nombre de usuario: ");
-        this.userName = scanner.nextLine();
-        System.out.println("Contraseña: ");
-        this.password = scanner.nextLine();
-        System.out.println("E-Mail: ");
-        this.emailAddress = scanner.nextLine();
-    }
-
     @Override
     public String toString(){
         return "Datos de usuario: " + "\n Nombre: " + this.name + "\n Apellido: " + this. surname +
@@ -151,7 +127,5 @@ public class User {
                 "\n Dirección : " + this.address + "\n Nombre de usuario : " + this.userName +
                 "\n Contraseña : " + this.password + "\n E-Mail : " + this.emailAddress;
     }
-
-
 
 }
