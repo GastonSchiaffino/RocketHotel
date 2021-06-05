26c0efed037620a9d79da9cc052c98bacc2ceb9b
LucasMostafa <mostafalucas@gmail.com>
2021-06-05 18:43:30 -0300

    Menu de usuario.
    Creando menu cliente.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,50 +3,118 @@ package com.company;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.awt.desktop.ScreenSleepEvent;
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         ///Menu
-        User user = new User();
-        Client client = new Client();
-       // user.register();
-        Scanner scanner= new Scanner(System.in);
-        boolean salir= false;
-        int opcion;
+        CollectionUser listUser = new CollectionUser();
+
+        Scanner scanner = new Scanner(System.in);
+
+        char character;
+
+        boolean quit = false;
+        int option= 0;
+
 
         do {
             try {
-                System.out.println("Menu: \n");
+                System.out.println("ROCKET HOTEL: \n");
                 System.out.println("1)Login.\n2)Registrarse.\n\nOpcion: ");
-                opcion = scanner.nextInt();
+                option = scanner.nextInt();
+                scanner.nextLine();
 
-                switch (opcion) {
+                switch (option) {
                     case 1 -> {
-                        System.out.println("LOGIN.\n");
-                        System.out.println(user.login());
-                        System.out.println(user.toString());
-                        salir = true;
+                        System.out.println("LOGIN.");
+                        do {
+                            System.out.println("\nNombre de usuario o E-mail: ");
+                            String userName = scanner.nextLine();
+                            System.out.println("\nPassword: ");
+                            String password = scanner.nextLine();
+                            User user = listUser.loginUser(userName, password);
+                            if (user != null) {
+                                System.out.println(user.toString());
+                                if (user instanceof Client) {
+                                    System.out.println("Usuario " + user.getUserName());
+                                    System.out.println("\n1)Ver habitaciones disponibles.\n2)Realizar reserva.\n3)Listados de reservas anteriores." +
+                                            "\n4)Cancelar reserva.\n5)Ver perfil.\n6)Modificar perfil.");
+                                    option = scanner.nextInt();
+                                    try{
+                                        switch (option){
+                                            case 1 -> {
+
+                                            }
+                                            case 2 -> {
+
+                                            }
+                                            case 3 -> {
+
+                                            }
+                                            case 4 -> {
+
+                                            }
+                                            case 5 -> {
+
+                                            }
+                                            case 6 -> {
+
+                                            }
+                                            default -> System.out.println("\nOpcion incorrecta.\n");
+                                            }
+                                     } catch (InputMismatchException e) {
+                                    System.out.println("\nSe debe ingresar un numero.\n");
+                                    scanner.next();
+                                }
+                                    }
+                                    scanner.nextLine();
+                                } else if (user instanceof Administrator) {
+
+                                } else {
+
+                                }
+
+
+                                quit = true;
+                            }
+                            else {
+                                System.out.println("\nUsuario / E-mail o contraseña incorrectos. Presione 'n' para salir o cualquier otra tecla para continuar.\n");
+                                character = scanner.next().charAt(0);
+                                if(character=='n'){
+                                    quit= true;
+                                }
+                            }
+
+                        }
+                        while (!quit);
                     }
                     case 2 -> {
                         System.out.println("REGISTRARSE.\n");
-                        salir = true;
+                        quit = true;
+                    }
+                    case 0 -> {
+                        quit = true;
                     }
                     default -> System.out.println("\nOpcion incorrecta.\n");
                 }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("\nSe debe ingresar un numero.\n");
                 scanner.next();
             }
         }
-        while(!salir);
+        while (!quit && option == 0);
 
     }
-
 }
